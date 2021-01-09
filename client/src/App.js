e1e6412c7bf7c654f3f9e406d476fdbf860742eb
hameed kazeem <hameed.kazeem@gmail.com>
2021-01-09 18:18:51 -0500

    added stripe feature to process payment

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
-
+import Success from "./pages/Success";
 const client = new ApolloClient({
   request: (operation) => {
     const token = localStorage.getItem('id_token')
@@ -37,6 +37,8 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" component={Success} />
+
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
