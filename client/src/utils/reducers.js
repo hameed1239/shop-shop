21e889820dcb803cc65f2c57715a7b9b5668143e
hameed kazeem <hameed.kazeem@gmail.com>
2021-01-09 12:43:23 -0500

    added cart UI and functionality

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,7 +1,13 @@
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
 } from "./actions";
 import { useReducer } from 'react';
 
@@ -13,17 +19,69 @@ export const reducer = (state, action) => {
                 ...state,
                 products: [...action.products],
             };
-        // If action value is the value of "UPDATE_CATEGORIES", return na new state object with an updated categories array
+        // If action value is the value of "UPDATE_CATEGORIES", return a new state object with an updated categories array
         case UPDATE_CATEGORIES:
             return {
                 ...state,
                 categories: [...action.categories]
             };
-        // If action value is the value of "UPDATE_CURRENT_CATEGORY", return na new state object with an updated current category
+        // If action value is the value of "UPDATE_CURRENT_CATEGORY", return a new state object with an updated current category
         case UPDATE_CURRENT_CATEGORY:
             return {
                 ...state,
                 currentCategory: action.currentCategory
+            };
+        //if action value is the value of "ADD_TO_CART", return a new state object with an updated cart list
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product]
+            };
+        //if action value is the value of "ADD_MULTIPLE_TO_CART", return a new state object with an updated cart list
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, ...action.products]
+            };
+        //if action value is the value of "REMOVE_FROM_CART", return a new state object with an updated cart list
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(product => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            };
+        //if action value is the value of "UPDATE_CART_QUANTITY", return a new state object with an updated cart list
+        case UPDATE_CART_QUANTITY:
+            let newState1 = state.cart.map(product => {
+                if (product._id === action._id) {
+                    product.purchaseQuantity = action.purchaseQuantity
+                }
+                return product;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState1.length > 0,
+                cart: newState1
+            };
+        case CLEAR_CART:
+
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            };
+        case TOGGLE_CART:
+
+            return {
+                ...state,
+                cartOpen: !state.cartOpen
             };
         // if it's none of these actions, do not update state at all and keep things the same!
         default:
